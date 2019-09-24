df5806d17e710d4dc1e9828862494d9faaef871e
Taufiq R <taufiq.it2@gmail.com>
2019-09-24 19:04:07 +0700

    first commit

---
+++ src/pbo/pkg10118080/latihan1/biodata/PBO10118080Latihan1Biodata.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118080.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * Nama :Taufiq Rizky
+ * NIM  :10118080
+ * Deskripsi Program    : program menampilkan biodata
+ */
+public class PBO10118080Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA PRGRAMMER"+"\n"+"--------------------");
+        System.out.println("Nama\t\t: Taufiq Rizky Darmawan S");
+        System.out.println("ALAMAT\t\t: Jalan Taman Saturnus 1 No.45");
+        System.out.println("Tempat Lahir\t: Bandung");
+        System.out.println("Tanggal Lahir\t: 19 agustus 2000");
+        System.out.println("--------------------");
+        System.out.println("Hobi\t\t: Ngoding");
+        System.out.println("Makes\t\t: Rendang ");
+        System.out.println("Mikes\t\t: Air Mineral");
+    }
+    
+}
